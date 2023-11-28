5a865ff6b64ab3ea6e35277c242ffb4076efff99
Bruno Melo <c.brunomelo@outlook.com>
2023-11-27 21:42:20 -0300

    criado metodo hash

--- TaskManager/Services/Entitys/User.vb
+++ TaskManager/Services/Entitys/User.vb
@@ -1,4 +1,8 @@
-﻿Namespace TaskManager.Services.Entitys
+﻿Imports System.Runtime.InteropServices
+Imports System.Security.Cryptography
+Imports System.Text
+
+Namespace TaskManager.Services.Entitys
 
     Public NotInheritable Class User
         Inherits Entity
@@ -13,7 +17,7 @@
         Sub New(id As Integer, name As String, passwordHash As String)
             Me.Id = id
             Me.Name = name
-            Me.PasswordHash = passwordHash
+            Me.PasswordHash = HashPassword(passwordHash)
         End Sub
 
         Sub New(name As String, passwordHash As String)
@@ -21,6 +25,18 @@
             Me.PasswordHash = passwordHash
         End Sub
 
+
+        Private Function HashPassword(password As String) As String
+            Dim data = Encoding.UTF8.GetBytes(password)
+            Dim sha256 As SHA256 = SHA256.Create()
+            Dim hashbytes As Byte() = sha256.ComputeHash(data)
+            Dim stringBuilder As New StringBuilder()
+            For Each b In hashbytes
+                stringBuilder.Append(b.ToString("x2"))
+            Next
+            Return stringBuilder.ToString()
+
+        End Function
 
 
     End Class
