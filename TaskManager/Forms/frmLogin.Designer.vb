948eb35477470d2a8070b13c38454a345cf663f2
Bruno Melo <c.brunomelo@outlook.com>
2023-11-29 20:50:03 -0300

    criado form para login

---
+++ TaskManager/Forms/frmLogin.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblLogin = New System.Windows.Forms.Label()
+        Me.lblSenha = New System.Windows.Forms.Label()
+        Me.txtLogin = New System.Windows.Forms.TextBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnCriar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblLogin
+        '
+        Me.lblLogin.AutoSize = True
+        Me.lblLogin.Location = New System.Drawing.Point(58, 105)
+        Me.lblLogin.Name = "lblLogin"
+        Me.lblLogin.Size = New System.Drawing.Size(33, 13)
+        Me.lblLogin.TabIndex = 0
+        Me.lblLogin.Text = "Login"
+        '
+        'lblSenha
+        '
+        Me.lblSenha.AutoSize = True
+        Me.lblSenha.Location = New System.Drawing.Point(58, 159)
+        Me.lblSenha.Name = "lblSenha"
+        Me.lblSenha.Size = New System.Drawing.Size(38, 13)
+        Me.lblSenha.TabIndex = 1
+        Me.lblSenha.Text = "Senha"
+        '
+        'txtLogin
+        '
+        Me.txtLogin.Location = New System.Drawing.Point(97, 105)
+        Me.txtLogin.Name = "txtLogin"
+        Me.txtLogin.Size = New System.Drawing.Size(146, 20)
+        Me.txtLogin.TabIndex = 2
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Location = New System.Drawing.Point(97, 159)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.Size = New System.Drawing.Size(146, 20)
+        Me.txtSenha.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(119, 211)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(87, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Entrar"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnCriar
+        '
+        Me.btnCriar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnCriar.FlatAppearance.BorderSize = 0
+        Me.btnCriar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCriar.Location = New System.Drawing.Point(97, 251)
+        Me.btnCriar.Name = "btnCriar"
+        Me.btnCriar.Size = New System.Drawing.Size(146, 23)
+        Me.btnCriar.TabIndex = 5
+        Me.btnCriar.Text = "Novo Usúario"
+        Me.btnCriar.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(318, 295)
+        Me.Controls.Add(Me.btnCriar)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtLogin)
+        Me.Controls.Add(Me.lblSenha)
+        Me.Controls.Add(Me.lblLogin)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmLogin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblLogin As Label
+    Friend WithEvents lblSenha As Label
+    Friend WithEvents txtLogin As TextBox
+    Friend WithEvents txtSenha As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnCriar As Button
+End Class
